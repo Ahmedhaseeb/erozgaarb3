266fb6be435053e659177d749de25977cc4a0640
Ahmedhaseeb <ahmedhaseeb123@gmail.com>
2018-10-03 10:46:35 +0500

    JavaScript Basics Part II
    JavaScript Basics Part II

---
+++ javascript/assets/js/functions.js
@@ -0,0 +1,13 @@
+var x = 50;
+function sum(num1,num2) {
+var i = 10;
+
+ return num1 + num2;   	
+}
+
+// name =  prompt("Please enter your name");
+// console.log(name);
+// sum  = sum(10,20);
+// console.log(sum);
+// console.log(x);
+// //console.log(i);
---
+++ javascript/assets/js/loops.js
@@ -0,0 +1,45 @@
+// // ++x , x++
+
+// // postfix
+// x = 20;
+// test = x++;
+// console.log(test);
+// console.log(x);
+
+// //prefix
+// x = 20;
+// test = ++x;
+// console.log(test);
+// console.log(x);
+
+// var x = 1;
+// while (x <= 10) {
+// 	console.log(x);
+// 	x++;
+// }
+
+
+// y = 1;
+
+// do {
+// 	console.log(y);
+// 	y++;
+// }while(y <= 20);
+
+
+
+// for(var i = 0;i<10;i++){
+// 	console.log(i);
+// }
+
+
+
+// arr = [1,2,3,4];
+
+// for(i=0;i<arr.length;i++){
+// 	console.log(arr[i]);
+// }
+
+// for(value of arr){
+// 	console.log(value);
+// }
